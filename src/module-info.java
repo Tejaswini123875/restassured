11093f7f27696ddbed7aad17918fcad8283eaaae
“Tejaswini123875” <aanjanatejaswini@gmail.com>
2023-05-31 19:22:05 +0530

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 2174324
+ *
+ */
+module restAsssured {
+}
---
+++ src/restAsssured/Basic.java
@@ -0,0 +1,12 @@
+package restAsssured;
+import static io.restassured.RestAssured.*;
+
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        RestAssured.
+	}
+
+}
